a7d204f547c7c3811dc4fdf4ab93660a1c51944a
Leandro Ortiz <ortiz.lean.uy@gmail.com>
2021-02-23 18:01:13 -0300

    bug fix in ternary operator it should return null

--- src/components/OrganisationEmployees/LocationDetail/LocationDetail.jsx
+++ src/components/OrganisationEmployees/LocationDetail/LocationDetail.jsx
@@ -73,7 +73,7 @@ export default class CompanyDetail extends React.Component {
                     </Panel>
 
                 </div>
-                : {}
+                : null
             }
             </>
         )
